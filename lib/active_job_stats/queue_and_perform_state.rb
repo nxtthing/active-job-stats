2a32493cb8619c440fd5bfe17c6f5eca4a364c36
alexyakubenko <alexandr.yakubenko@gmail.com>
2026-01-21 17:13:06 -0600

    get rid of class vars

--- lib/active_job_stats/queue_and_perform_state.rb
+++ lib/active_job_stats/queue_and_perform_state.rb
@@ -76,7 +76,7 @@ module ActiveJobStats
       end
 
       def job_stats_expiration_time
-        @job_stats_expiration_time || 20.minutes
+        20.minutes
       end
 
       def combine_perform_state_keys(keys)
@@ -100,10 +100,18 @@ module ActiveJobStats
       ).any?
     end
 
+    def set_job_stats_expiration_time(value)
+      define_method(:job_stats_expiration_time) { value }
+    end
+
+    def ignore_job_key_in_perform_state_key!
+      define_singleton_method(:job_key_in_perform_state_key?) { false }
+    end
+
     private
 
     def job_key_in_perform_state_key?
-      @job_key_in_perform_state_key || true
+      true
     end
   end
 end
